634be103a20c3832648247a765f0aa3a10950d42
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2022-02-26 15:44:04 +0600

    resume updated

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -41,6 +41,63 @@ class Resume extends Component {
       );
     });
 
+    const achievements = this.props.data.achievements.map(function (
+      achievements
+    ) {
+      return (
+        <div key={achievements.name}>
+          <h3>{achievements.name}</h3>
+          <p className="info">
+            {achievements.info} <span>&bull;</span>
+            <em className="date">{achievements.year}</em>
+          </p>
+          <p>{achievements.description}</p>
+        </div>
+      );
+    });
+
+    const coCurricular = this.props.data.coCurricular.map(function (
+      coCurricular
+    ) {
+      return (
+        <div key={coCurricular.name}>
+          <h3>{coCurricular.name}</h3>
+          <p className="info">
+            <em className="date">{coCurricular.year}</em>
+          </p>
+          <p>{coCurricular.description}</p>
+        </div>
+      );
+    });
+
+    const extraCurricular = this.props.data.extraCurricular.map(function (
+      extraCurricular
+    ) {
+      return (
+        <div key={extraCurricular.name}>
+          <h3>{extraCurricular.name}</h3>
+          <p className="info">
+            <em className="date">{extraCurricular.year}</em>
+          </p>
+          <p>{extraCurricular.description}</p>
+        </div>
+      );
+    });
+
+    const reference = this.props.data.reference.map(function (
+      reference
+    ) {
+      return (
+        <div key={reference.name}>
+          <h3>{reference.name}</h3>
+          <p className="info">
+            <em className="date">{reference.designition}</em>
+          </p>
+          <p>{reference.contact}</p>
+        </div>
+      );
+    });
+
     const skills = this.props.data.skills.map((skills) => {
       const backgroundColor = this.getRandomColor();
       const className = "bar-expand " + skills.name.toLowerCase();
@@ -54,8 +111,36 @@ class Resume extends Component {
       );
     });
 
+    const languages = this.props.data.languages.map((languages) => {
+      return (
+        <li key={languages.name}>
+          <em>{languages.name}</em>
+        </li>
+      );
+    });
+
+    const interests = this.props.data.interests.map((interests) => {
+      return (
+        <li key={interests.name}>
+          <em>{interests.name}</em>
+        </li>
+      );
+    });
+
     return (
       <section id="resume">
+        <Slide left duration={1300}>
+          <div className="row work">
+            <div className="three columns header-col">
+              <h1>
+                <span>Work Experience</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">{work}</div>
+          </div>
+        </Slide>
+
         <Slide left duration={1300}>
           <div className="row education">
             <div className="three columns header-col">
@@ -73,14 +158,18 @@ class Resume extends Component {
         </Slide>
 
         <Slide left duration={1300}>
-          <div className="row work">
+          <div className="row education">
             <div className="three columns header-col">
               <h1>
-                <span>Work</span>
+                <span>Achievements</span>
               </h1>
             </div>
 
-            <div className="nine columns main-col">{work}</div>
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{achievements}</div>
+              </div>
+            </div>
           </div>
         </Slide>
 
@@ -97,6 +186,80 @@ class Resume extends Component {
 
               <div className="bars">
                 <ul className="skills">{skills}</ul>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Co-curricular</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{coCurricular}</div>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Extra-curricular</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{extraCurricular}</div>
+              </div>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row skill">
+            <div className="three columns header-col">
+              <h1>
+                <span>languages</span>
+              </h1>
+            </div>
+            <div className="nine columns main-col">
+              <ul className="skills">{languages}</ul>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row skill">
+            <div className="three columns header-col">
+              <h1>
+                <span>Interests</span>
+              </h1>
+            </div>
+            <div className="nine columns main-col">
+              <ul className="skills">{interests}</ul>
+            </div>
+          </div>
+        </Slide>
+
+        <Slide left duration={1300}>
+          <div className="row education">
+            <div className="three columns header-col">
+              <h1>
+                <span>Reference</span>
+              </h1>
+            </div>
+
+            <div className="nine columns main-col">
+              <div className="row item">
+                <div className="twelve columns">{reference}</div>
               </div>
             </div>
           </div>
